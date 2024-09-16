68ef4f024585b2be6bd66d6f7d0bcf4a5f0c0be2
sakshiOfficail1992 <sakshiofficial1992@gmail.com>
2024-09-16 19:15:27 +0530

    String Learning

---
+++ 05_String.js
@@ -0,0 +1,48 @@
+ 
+ let name = "dhwani"
+ let repoCount = 40
+
+//console.log(name + repoCount + " Value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String('dhwani-baby-princess')
+
+console.log(gamename[2]);
+console.log(gamename.__proto__);
+
+// console.log(gamename. length);
+
+//console.log(gamename.toUpperCase());
+console.log(gamename.charAt(5));
+//console.log(gamename.indexOf('a'));
+
+const newString = gamename.substring(0, 6)
+console.log(newString);
+
+const anotherString = gamename.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "    Dhwani   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://dhwani.com/dhwani%50chaudhary"
+console.log(url.replace('%50',  '-'));
+
+console.log(url.includes('dhwani'));
+
+console.log(url.includes('baby'));
+
+console.log(gamename.split('-'));
+
+
+
+
+const StringOne = "Dhwani"
+const StringTwo = "Vaanu"
+let StringThree = StringOne.concat(" is sister of ", StringTwo )
+console.log(StringThree);
+
+
+
+
---
+++ Memory_Types.js
@@ -0,0 +1,12 @@
+
+let num = {obj: "num" };
+let num2 = num
+num2.obj="num2" ;
+//console.log(num);
+console.log(num);
+console.log(num2);
+
+
+
+
+
